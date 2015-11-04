fdcb6ea083df2eabc16492f417bb55e71718c79c
Roberto Soares <robertomelo822@gamil.com>
2015-11-03 22:45:32 -0200

    Reparo

--- resources/views/partials/contato.blade.php
+++ resources/views/partials/contato.blade.php
@@ -1,7 +1,7 @@
 
 <div class="panel panel-default">
   <div class="panel-heading">
-    <h3 class="panel-title">{{$pessoa->apelido}}</h3>
+    <h3 class="panel-title">{{$pessoa->apelido }}</h3>
   </div>
   <div class="panel-body">
       <h1></h1>
